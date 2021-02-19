71bf4510fb7e7413649b5ec9eda849486bd8f6d8
maged magdy <maged.magdy@talentsarena.net>
2021-02-19 18:48:47 +0200

    - Add the basic skeleton to our app in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello");
+import express from "express";
+
+const app: express.Application = express();
+const PORT: number = 8000;
+
+app.get("/", (req, res) => {
+  res.send("hello there!");
+});
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
