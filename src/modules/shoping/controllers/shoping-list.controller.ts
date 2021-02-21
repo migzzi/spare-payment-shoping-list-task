bf0948d76918a4876d50a968ef5d95a60dc16fb3
maged magdy <maged.magdy@talentsarena.net>
2021-02-21 12:31:30 +0200

    - Add shoping list item add endpoint.

--- src/modules/shoping/controllers/shoping-list.controller.ts
+++ src/modules/shoping/controllers/shoping-list.controller.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import db from '../../../db';
+import db, { getNextId } from '../../../db';
+import { transfereObject } from '../../../utils/object';
 
 export const ShopingListRouter: Router = Router();
 
@@ -20,5 +21,52 @@ ShopingListRouter.get('/shoping-list', (req, res) => {
 	return res.json({
 		shopingList,
 		total: shopingList.reduce((total, item) => total + item.total_price, 0)
+	});
+});
+
+ShopingListRouter.put('/shoping-list/item/add', (req, res) => {
+	let productId = parseInt(req.body.product_id),
+		productToAdd = db.products.find(p => p.id === productId),
+		requestedQty = parseInt(req.body.quantity);
+
+	// Case of invalid product
+	if (!productToAdd)
+		return res.json({
+			success: false,
+			message: `No product with id #${productId} was found.`
+		});
+	// Case of not enough quantity in stock
+	if (productToAdd.quantity < requestedQty)
+		return res.json({
+			success: false,
+			message: 'No enough quantity available in stock.'
+		});
+
+	// The case when adding a product that has already been added before.
+	// The requirements didn't specify how to handle such case so I'will just imporvicse
+	// and will update the quantity of the already selected item.
+	let item = db.shopingList.find(item => item.product_id === productToAdd.id);
+	if (item) {
+		transfereObject(item, { quantity: requestedQty });
+		// If item already exists then just deduct/add the difference between old and new quantities.
+		transfereObject(productToAdd, { quantity: productToAdd.quantity - (requestedQty - item.quantity) });
+		return res.json({
+			success: true,
+			message: "Updated selected item's quantity."
+		});
+	}
+
+	// Deduct the requested quantity from the stock.
+	transfereObject(productToAdd, { quantity: productToAdd.quantity - requestedQty });
+
+	db.shopingList.push({
+		id: getNextId('shopingList'),
+		product_id: productId,
+		quantity: requestedQty
+	});
+
+	return res.json({
+		success: true,
+		message: 'Item added successfully.'
 	});
 });
