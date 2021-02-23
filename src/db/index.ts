9c3c5b85ea816c904e61946fcac992fed99d9317
maged magdy <maged.magdy@talentsarena.net>
2021-02-23 17:35:00 +0200

    --- HOT FIX --- - Add getNextId function (forgot to commit it).

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,16 @@
-import products from "./products";
+import products from './products';
 
 const db: {
-  [prop: string]: any[];
+	[prop: string]: any[];
 } = {
-  products,
-  shopingList: [],
+	products,
+	shopingList: []
 };
 
 export default db;
+
+export function getNextId(tableName: string): number {
+	let table = db[tableName];
+	if (!table) throw new Error(`Table ${tableName} not found in database.`);
+	return Math.max(...table.map(r => r.id)) + 1;
+}
